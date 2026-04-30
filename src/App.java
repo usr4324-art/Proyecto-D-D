c3207da05b09068e69d32c5de0ad83933e08784c
LujánEspinoza <usr4324@salesianos-lacuesta.net>
2026-04-30 13:48:49 +0100

    correccion de codigo (getters)

--- src/App.java
+++ src/App.java
@@ -6,7 +6,7 @@ public class App {
         Personaje guerrero = new Guerrero(100, 20, 10, 5, 50, 30);
         Personaje mago = new Mago(80, 15, 5, 7, 100, 40);
 
-        System.out.println("Hola, soy un guerrero con vida: " + ((Guerrero) guerrero).getVida() + " y furia: " + ((Guerrero) guerrero).getFuria());
-        System.out.println("Hola, soy un mago con vida: " + ((Mago) mago).getVida() + " y poder mágico: " + ((Mago) mago).getPoderMagico());
+        System.out.println("Hola, soy un guerrero con armadura: " + ((Guerrero) guerrero).getArmadura() + " y furia: " + ((Guerrero) guerrero).getFuria());
+        System.out.println("Hola, soy un mago con mana: " + ((Mago) mago).getMana() + " y poder mágico: " + ((Mago) mago).getPoderMagico());
     }
 }
