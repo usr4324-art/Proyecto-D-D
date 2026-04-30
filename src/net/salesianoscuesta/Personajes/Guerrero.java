26f1095cd2dddbf2da2010e6f17f576bf2646631
LujánEspinoza <usr4324@salesianos-lacuesta.net>
2026-04-30 13:26:25 +0100

    Crear atributos + constructor Guerrero

---
+++ src/net/salesianoscuesta/Personajes/Guerrero.java
@@ -0,0 +1,13 @@
+package net.salesianoscuesta.Personajes;
+import net.salesianoscuesta.Personaje.Personaje;
+
+public class Guerrero extends Personaje {
+   private int furia;
+   private int armadura;
+
+    public Guerrero(int vida, int ataque, int defensa, int velocidad, int furia, int armadura) {
+        super(vida, ataque, defensa, velocidad);
+        this.furia = furia;
+        this.armadura = armadura;
+    }
+}
