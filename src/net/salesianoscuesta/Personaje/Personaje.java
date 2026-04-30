9f8f27aed19eda94bb2850acc711636055e4f77d
LujánEspinoza <usr4324@salesianos-lacuesta.net>
2026-04-30 13:22:34 +0100

    Crear atributos + constructor en Personaje

--- src/net/salesianoscuesta/Personaje/Personaje.java
+++ src/net/salesianoscuesta/Personaje/Personaje.java
@@ -1,5 +1,15 @@
 package net.salesianoscuesta.Personaje;
 
 public class Personaje {
-    
+    private int  vida;
+    private int  ataque;
+    private int  defensa;
+    private int  velocidad;
+
+    public Personaje(int vida, int ataque, int defensa, int velocidad) {
+        this.vida = vida;
+        this.ataque = ataque;
+        this.defensa = defensa;
+        this.velocidad = velocidad;
+    }
 }
